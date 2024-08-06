08dbeba1e6125b9d5bf66b7a0e142b4a815e8cb1
jpfagiani <61874180+jpfagiani@users.noreply.github.com>
2024-08-06 12:37:10 -0300

    Update storage.sh

--- storage.sh
+++ storage.sh
@@ -18,7 +18,7 @@ apt install openssh-server xfsprogs -y
 	mkdir -pv /srv/supervisao /srv/disciplina /srv/pulbico
 		echo LABEL=supervisao /srv/supervisao ext4 defaults 0 0 >> /etc/fstab
 		echo LABEL=disciplina /srv/disciplina ext4 defaults 0 0 >> /etc/fstab
-		echo LABEL=publico    /srv/pulbico    ext4 defaults 0 0 >> /etc/fstab
+		echo LABEL=publico    /srv/publico    ext4 defaults 0 0 >> /etc/fstab
 for i in .homes .drivers .lixeiras publico; do mkdir -pv /srv/publico/$i; done 
 for i in sindicancia supervisao cimic; do mkdir -pv /srv/supervisao/$i; done
 for i in chefia_turno_I chefia_turno_II chefia_turno_III chefia_turno_IV conexao_familiar administrativo aevp almoxarifado canil  cpd dcsd educacao financas inclusao infraestrutura  saude ; do mkdir -pv /srv/disciplina/$i; done
