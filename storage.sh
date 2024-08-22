c776692ddc6b5897e3695780561475461c0f3f97
jpfagiani <61874180+jpfagiani@users.noreply.github.com>
2024-08-22 14:58:47 -0300

    Update storage.sh

--- storage.sh
+++ storage.sh
@@ -2,10 +2,11 @@
 
 hostnamectl set-hostname storage.cdpni.gov
 apt install openssh-server xfsprogs -y
-	cp -v /root/cdpni/storage/network/interfaces /etc/network/
-	cp -v /root/cdpni/storage/etc/* /etc/
+apt install nfs-common nfs-kernel-server -y
+	systemctl start nfs-server
+	systemctl enable nfs-server                                                                                                                                                                                               	
+	cp -rv /root/storage/etc/* /etc/
 	chattr +i /etc/resolv.conf
-	cp -v /root/cdpni/storage/ssh/sshd_config /etc/ssh/
 	systemctl restart networking sshd
 	#mkfs -t xfs /dev/sdb1
 	#	xfs-admin -L supervisao /dev/sdb1 
@@ -18,7 +19,7 @@ apt install openssh-server xfsprogs -y
 	mkdir -pv /srv/supervisao /srv/disciplina /srv/pulbico
 		echo LABEL=supervisao /srv/supervisao ext4 defaults 0 0 >> /etc/fstab
 		echo LABEL=disciplina /srv/disciplina ext4 defaults 0 0 >> /etc/fstab
-		echo LABEL=publico    /srv/publico    ext4 defaults 0 0 >> /etc/fstab
+		echo LABEL=publico    /srv/pulbico    ext4 defaults 0 0 >> /etc/fstab
 for i in .homes .drivers .lixeiras publico; do mkdir -pv /srv/publico/$i; done 
 for i in sindicancia supervisao cimic; do mkdir -pv /srv/supervisao/$i; done
 for i in chefia_turno_I chefia_turno_II chefia_turno_III chefia_turno_IV conexao_familiar administrativo aevp almoxarifado canil  cpd dcsd educacao financas inclusao infraestrutura  saude ; do mkdir -pv /srv/disciplina/$i; done
