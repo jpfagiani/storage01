2b98110911c5411545aae836d4a380c55ca4ae6d
jpfagiani <61874180+jpfagiani@users.noreply.github.com>
2024-08-24 12:28:20 -0300

    Update nfsexport.sh

--- nfsexport.sh
+++ nfsexport.sh
@@ -3,13 +3,17 @@
 apt install nfs-common nfs-kernel-server -y
 systemctl start nfs-server
 systemctl enable nfs-server
-for i in .homes .drivers .lixeiras publico;  do echo -e "/srv/samba/$i  \t\t 10.14.29.0/24(rw,no_root_squash,no_subtree_check)" >> /etc/exports; done
-for i in sindicancia supervisao cimic;  do echo -e "/srv/samba/$i  \t\t 10.14.29.0/24(rw,no_root_squash,no_subtree_check)" >> /etc/exports; done
-for i in chefia_turno_I chefia_turno_II chefia_turno_III chefia_turno_IV conexao_familiar administrativo aevp almoxarifado canil cpd dcsd educacao financas inclusao infraestrutura  saude; do echo -e "/srv/samba/$i  \t\t 10.14.29.0/24(rw,no_root_squash,no_subtree_check)" >> /etc/exports; done
-for i in scanner wallpaper diretoria_geral diretoria_de_centro nucleo_de_pessoal portaria_turno_I portaria_turno_II portaria_turno_III portaria_turno_IV rol_de_visitas; do echo -e "/srv/samba/$i  \t\t 10.14.29.0/24(rw,no_root_squash,no_subtree_check)" >> /etc/exports; done
+for i in .homes .drivers .lixeiras publico; 
+      do echo -e "/srv/samba/$i  \t\t 10.14.29.0/24(rw,no_root_squash,no_subtree_check)" >> /etc/exports; done
+for i in sindicancia supervisao cimic; 
+      do echo -e "/srv/samba/$i  \t\t 10.14.29.0/24(rw,no_root_squash,no_subtree_check)" >> /etc/exports; done
+for i in chefia_turno_I chefia_turno_II chefia_turno_III chefia_turno_IV conexao_familiar administrativo aevp almoxarifado canil cpd dcsd educacao financas inclusao infraestrutura  saude;
+      do echo -e "/srv/samba/$i  \t\t 10.14.29.0/24(rw,no_root_squash,no_subtree_check)" >> /etc/exports; done
+for i in scanner wallpaper diretoria_geral diretoria_de_centro nucleo_de_pessoal portaria_turno_I portaria_turno_II portaria_turno_III portaria_turno_IV rol_de_visitas; 
+      do echo -e "/srv/samba/$i  \t\t 10.14.29.0/24(rw,no_root_squash,no_subtree_check)" >> /etc/exports; done
 exportfs -r
 # ou systemctl restart nfs-server
-showmount -e 10.14.29.12
+showmount -e 10.14.29.11
 
 
 
