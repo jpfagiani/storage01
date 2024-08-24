6612e5eafe5250d1db57ed1430c70408c2064ecd
jpfagiani <61874180+jpfagiani@users.noreply.github.com>
2024-08-24 12:31:06 -0300

    Update storage.sh

--- storage.sh
+++ storage.sh
@@ -24,7 +24,7 @@ for i in sindicancia supervisao cimic;
       do mkdir -pv /srv/supervisao/$i; done
 for i in chefia_turno_I chefia_turno_II chefia_turno_III chefia_turno_IV conexao_familiar administrativo aevp almoxarifado canil  cpd dcsd educacao financas inclusao infraestrutura saude ;
       do mkdir -pv /srv/disciplina/$i; done
-for i in scanner  wallpaper diretoria_geral diretoria_de_centro nucleo_de_pessoal portaria_turno_I portaria_turno_II portaria_turno_III portaria_turno_IV rol_de_visitas;
+for i in cipa scanner  wallpaper diretoria_geral diretoria_de_centro nucleo_de_pessoal portaria_turno_I portaria_turno_II portaria_turno_III portaria_turno_IV rol_de_visitas;
       do mkdir -pv /srv/disciplina/$i; done
 chmod 777 /srv/*
 
