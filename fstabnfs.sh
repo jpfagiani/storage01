248a220710edb9541875498dac50969681886e96
jpfagiani <61874180+jpfagiani@users.noreply.github.com>
2024-08-24 13:44:55 -0300

    Update fstabnfs.sh

--- fstabnfs.sh
+++ fstabnfs.sh
@@ -3,6 +3,6 @@
 for i in .homes .drivers .lixeiras .cpd publico sindicancia supervisao cimic chefia_turno_I chefia_turno_II chefia_turno_III chefia_turno_IV conexao_familiar administrativo aevp almoxarifado canil
 dcsd educacao financas inclusao infraestrutura saude cipa scanner wallpaper diretoria_geral diretoria_de_centro nucleo_de_pessoal portaria_turno_I portaria_turno_II portaria_turno_III
 portaria_turno_IV rol_de_visitas; 
-echo "10.14.29.11:/srv/samba/$i  /srv/samba/$i nfs defaults 0 0 " >> /etc/fstab ; 
+echo "10.14.29.11:/srv/samba/$i  /srv/samba/$i ext4 defaults 0 0 " >> /etc/fstab ; 
 chmod a+w /srv/samba/$i
 done
