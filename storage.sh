2dd5ca7cac6fc06e86e582290d9f33921788800d
jpfagiani <61874180+jpfagiani@users.noreply.github.com>
2024-08-24 12:29:57 -0300

    Update storage.sh

--- storage.sh
+++ storage.sh
@@ -18,10 +18,14 @@ apt install nfs-common nfs-kernel-server -y
 		echo LABEL=supervisao /srv/supervisao ext4 defaults 0 0 >> /etc/fstab
 		echo LABEL=disciplina /srv/disciplina ext4 defaults 0 0 >> /etc/fstab
 		echo LABEL=publico    /srv/publico    ext4 defaults 0 0 >> /etc/fstab
-for i in .homes .drivers .lixeiras publico; do mkdir -pv /srv/publico/$i; done 
-for i in sindicancia supervisao cimic; do mkdir -pv /srv/supervisao/$i; done
-for i in chefia_turno_I chefia_turno_II chefia_turno_III chefia_turno_IV conexao_familiar administrativo aevp almoxarifado canil  cpd dcsd educacao financas inclusao infraestrutura  saude ; do mkdir -pv /srv/disciplina/$i; done
-for i in scanner  wallpaper diretoria_geral diretoria_de_centro nucleo_de_pessoal portaria_turno_I portaria_turno_II portaria_turno_III portaria_turno_IV rol_de_visitas; do mkdir -pv /srv/disciplina/$i; done
+for i in .homes .drivers .lixeiras publico;
+      do mkdir -pv /srv/publico/$i; done 
+for i in sindicancia supervisao cimic;
+      do mkdir -pv /srv/supervisao/$i; done
+for i in chefia_turno_I chefia_turno_II chefia_turno_III chefia_turno_IV conexao_familiar administrativo aevp almoxarifado canil  cpd dcsd educacao financas inclusao infraestrutura saude ;
+      do mkdir -pv /srv/disciplina/$i; done
+for i in scanner  wallpaper diretoria_geral diretoria_de_centro nucleo_de_pessoal portaria_turno_I portaria_turno_II portaria_turno_III portaria_turno_IV rol_de_visitas;
+      do mkdir -pv /srv/disciplina/$i; done
 chmod 777 /srv/*
 
 
