633336b01fdeb1200d073eb3237ae9477bbb4af3
jpfagiani <61874180+jpfagiani@users.noreply.github.com>
2024-08-24 12:24:55 -0300

    Update mkdir.sh

--- mkdir.sh
+++ mkdir.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-for i in .homes .drivers .lixeiras publico sindicancia supervisao cimic chefia_turno_I chefia_turno_II chefia_turno_III chefia_turno_IV conexao_familiar administrativo aevp almoxarifado canil cpd dcsd educacao financas inclusao infraestrutura  saude ; do mkdir -pv /srv/disciplina/$i; done
-scanner wallpaper diretoria_geral diretoria_de_centro nucleo_de_pessoal portaria_turno_I portaria_turno_II portaria_turno_III portaria_turno_IV rol_de_visitas; do mkdir -pv /srv/samba/$i; done
+for i in .homes .drivers .lixeiras .cpd publico sindicancia supervisao cimic chefia_turno_I chefia_turno_II chefia_turno_III chefia_turno_IV conexao_familiar administrativo aevp almoxarifado canil
+dcsd educacao financas inclusao infraestrutura saude scanner wallpaper diretoria_geral diretoria_de_centro nucleo_de_pessoal portaria_turno_I portaria_turno_II portaria_turno_III
+portaria_turno_IV rol_de_visitas; do mkdir -pv /srv/samba/$i; done
 chmod a+w /srv/samba/*
